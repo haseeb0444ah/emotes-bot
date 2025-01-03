c8100e2e8f73d5f88f87177ec029bb6d56dbf7c9
haseeb0444ah <haseeb0222ah@gmail.com>
2025-01-03 23:48:10 +0500

    config.py

--- config/config.py
+++ config/config.py
@@ -1,14 +1,14 @@
 class config:
     # Basic configuration: If you are unsure how to obtain the Bot ID, simply start the bot and it will be logged in the console.
     prefix = '/'
-    botID = 'change-me'
-    botName = 'change-me'
-    ownerName = 'change-me'
-    roomName = 'change-me'
+    botID = '647de80cd0163e6dd3f3449c'
+    botName = 'SycoBot'
+    ownerName = '_HaseeB_'
+    roomName = 'Cheap Grabs'
     coordinates = {
-        'x': 8.5,
-        'y': 0.6000,
-        'z': 20.5,
+        'x': 17.5,
+        'y': 0.10000000149011612,
+        'z': 10.5,
         'facing': 'FrontRight'
     }
 
@@ -37,12 +37,12 @@ class messages:
 
 class permissions:
     # You can add as many IDs as you want, for example: ['id1', 'id2'].
-    owners = ['55bb64735531104341039ca8']
-    moderators = ['55bb64735531104341039ca8']
+    owners = ['636a7d9ecc79c4ca4f7da5ef','64c1e24c02e13e95a9374521','64c98e95c9b672a10bfd2405']
+    moderators = ['636a7d9ecc79c4ca4f7da5ef','64c1e24c02e13e95a9374521','64c98e95c9b672a10bfd2405']
 
 
 class authorization:
     # To obtain your token, visit https://highrise.game/ and log in. Then, go to the settings and create a new bot. Accept the terms and generate a token.
     # To obtain your room ID, go to the game and navigate to the top right corner where the player list is displayed. Click on "Share this room" and copy the ID.
-    room = 'change-me'
-    token = 'change-me'
+    room = '65130b5dbf60263900835226'
+    token = 'e0b1d9fc1cf8fcea5bd95a65f029576fd1e0a873c70771549c3d454d9db005f9'
